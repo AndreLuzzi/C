be30e756d88e18f6c9d73bd81272fb06c34c1f0d
AndreLuzzi <andreluzzi@outlook.com.br>
2024-09-12 16:51:41 -0300

    lista2

---
+++ Lista2/ex31.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float peso, emag, engor;
+    
+    printf("Qual o seu peso atual? \n");
+    scanf("%f", &peso);
+    engor = (peso * 1.15);
+    emag = (peso * 0.80);
+    printf("Se engordar ficara com %fKg e se emagrcer tera %fKg", engor, emag);
+    
+    return 0;
+}
