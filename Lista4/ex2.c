160af079db238910322b066c266c6d94ba3f2ab6
AndreLuzzi <andreluzzi@outlook.com.br>
2024-09-16 12:05:55 -0300

    lista4

---
+++ Lista4/ex2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    float numero, resultado;
+    int tcalculo;
+
+    printf("Digite o numero:\n");
+    scanf("%f", &numero);
+    printf("Selecione o tipo do calculo: 101-Raiz quadrada\n 102-A metade\n 103-10 porcento do numero\n 104-O dobro \n");
+    scanf("%d", &tcalculo);
+
+    if (tcalculo == 101) {
+        resultado = numero*numero;
+        printf("A raiz quadrada do seu numero e: %f\n", resultado);
+    } 
+    else if (tcalculo == 102) {
+        resultado = numero/2;
+        printf("A metade do seu numero e: %f\n", resultado);
+    } 
+    else if (tcalculo == 103) {
+        resultado = numero * 0.10;
+        printf("10% do seu numero e: %f\n", resultado);
+    } 
+    else if (tcalculo == 104) {
+        resultado = numero * 2;
+        printf("O dorbro do seu numero e: %f\n", resultado);
+    }
+
+    return 0;
+}
