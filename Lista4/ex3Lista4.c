0cb190bea234d695929fdb71cc2d7221bb2a853e
AndreLuzzi <andreluzzi@outlook.com.br>
2024-09-18 09:26:06 -0300

    lista4

--- Lista4/ex3Lista4.c
+++ Lista4/ex3Lista4.c
@@ -2,10 +2,9 @@
 
 int main() {
 
-    int menu;
+    int opcao;
 
-    printf("Bom dia!! Selecione o tipo do pagamento:\n 1-Venda a vista\n 2-Venda a Prazo 30 dias\n 3-Venda a Prazo 60 dias\n 4-Venda a Prazo com 90 dias\n 5-Venda com cartão de débito 
-    \n 6-Venda com cartao de credito");
+    printf("Bom dia!! Selecione o tipo do pagamento:\n 1-Venda a vista\n 2-Venda a Prazo 30 dias\n 3-Venda a Prazo 60 dias\n 4-Venda a Prazo com 90 dias\n 5-Venda com cartão de débito\n 6-Venda com cartao de credito");
     scanf("%d", &menu);
 
     if (menu == 1) {
