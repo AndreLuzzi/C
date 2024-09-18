3467f985262ee251f7c6eefd623376c8b33299f2
AndreLuzzi <andreluzzi@outlook.com.br>
2024-09-18 09:03:05 -0300

    lista4

--- Lista4/ex2.c
+++ Lista4/ex2.c
@@ -7,7 +7,7 @@ int main() {
 
     printf("Digite o numero:\n");
     scanf("%f", &numero);
-    printf("Selecione o tipo do calculo: 101-Raiz quadrada\n 102-A metade\n 103-10 porcento do numero\n 104-O dobro \n");
+    printf("Selecione o tipo do calculo:\n 101-Raiz quadrada\n 102-A metade\n 103-10 porcento do numero\n 104-O dobro \n");
     scanf("%d", &tcalculo);
 
     if (tcalculo == 101) {
@@ -20,7 +20,7 @@ int main() {
     } 
     else if (tcalculo == 103) {
         resultado = numero * 0.10;
-        printf("10% do seu numero e: %f\n", resultado);
+        printf("10 porcento do seu numero e: %f\n", resultado);
     } 
     else if (tcalculo == 104) {
         resultado = numero * 2;
